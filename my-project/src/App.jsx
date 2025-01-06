e406d3154338a9eda6faa13be35716dfca7f5dfe
Fudail Mohammed Zafar <fudail.zafar@gmail.com>
2025-01-06 11:54:20 +0530

    Navbar component
    developed the navbar component using TailwindCSS and changed the theme

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Example from "./components/Navbar";
 
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Example />
     </div>
   );
 };
