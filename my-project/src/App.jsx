841d0a1bf9b6dddc997a5c0f251cc6bd949c3dab
Fudail Mohammed Zafar <fudail.zafar@gmail.com>
2025-01-06 13:43:50 +0530

    updating navbar with another project
    updating the carousel with another project.

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import React from "react";
 import Example from "./components/Navbar";
 import Carousel from "./components/Carousel";
@@ -9,12 +10,38 @@ const App = () => {
     "https://wallpaperaccess.com/full/809523.jpg",
     "https://getwallpapers.com/wallpaper/full/5/c/0/606489.jpg",
   ];
+
+  let heroData = [
+    { text1: "Dive into", text2: "what you love" },
+    { text1: "Indulge", text2: "your passion" },
+    { text1: "Give in to", text2: "your passions" },
+  ];
+  const [heroCount, setHeroCount] = useState(0);
+  const [playStatus, setPlayStatus] = useState(false);
+
+  useEffect(() => {
+    setInterval(() => {
+      setHeroCount((count) => {
+        return count === 2 ? 0 : count + 1;
+      });
+    }, 3000);
+  }, []);
   return (
     <div>
       <Example />
       <div className="w-[60%] m-auto pt-11">
         <Carousel slides={slides} />
       </div>
+
+      <Background playStatus={playStatus} heroCount={heroCount} />
+      {/* <Navbar /> */}
+      <Hero
+        setPlayStatus={setPlayStatus}
+        heroData={heroData[heroCount]}
+        heroCount={heroCount}
+        setHeroCount={setHeroCount}
+        playStatus={playStatus}
+      />
     </div>
   );
 };
