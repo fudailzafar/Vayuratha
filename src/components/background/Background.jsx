c0d4ce4d35e1b0c0bec38d8a5e4f022ec4c405dd
Fudail Mohammed Zafar <fudail.zafar@gmail.com>
2025-01-06 16:50:15 +0530

    Update Background.jsx
    changed file name for deploying to vercel

--- src/components/background/Background.jsx
+++ src/components/background/Background.jsx
@@ -1,4 +1,4 @@
-import "./Background.css";
+import "./background.css";
 import video1 from "../../assets/video1.mp4";
 import image1 from "../../assets/image1.png";
 import image2 from "../../assets/image2.jpg";
